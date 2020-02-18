633b60122f2f7eacd0c0cfa7fd782c16bc0b712b
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-02-18 19:53:09 +0700

    1-0 Add draft

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+#include <stdin.h>
+
+char* ask_string(FILE* fin);
+
+int find_substr(char* const substr, char* const str, FILE* fout);
 
 int main(void) {
-    return EXIT_SUCCESS;
+  FILE* fin = fopen("in.txt", "r");
+  char *substr = ask_string(fin);
+  char *str = ask_string(fin);
+  fclose(fin);
+  if (substr == NULL || str == NULL)
+    return 1;
+  FILE* fout = fopen("out.txt", "w");
+  int const status = find_substr(substr, str, fout);
+  fclose(fout);
+  if (status != 0)
+    return 2;
+  return 0;
 }
