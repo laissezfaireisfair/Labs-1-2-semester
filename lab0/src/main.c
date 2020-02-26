1705cd2ff232173b417815d5c10cc5644d145d2d
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-02-26 13:04:07 +0700

    0 Add initialiser and deleter to Num

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -7,7 +7,8 @@ unsigned int const MAX_BASE      = 16;
 unsigned int const MIN_BASE      = 2;
 unsigned int const INPUT_MAX_LEN = 13;
 
-enum errors {OK, DIVISION_BY_ZERO, NULL_POINTER, INVALID_ARGUMENT, BAD_INPUT};
+enum errors {OK, DIVISION_BY_ZERO, NULL_POINTER, INVALID_ARGUMENT, BAD_INPUT,
+	 						RUNTIME_ERROR};
 typedef enum errors error;
 
 typedef struct _Num {
@@ -17,6 +18,25 @@ typedef struct _Num {
   unsigned int* bodyFrac;
   unsigned int lenFrac;
 } Num;
+
+error init_num(Num *num) {
+	if (num == NULL)
+		return NULL_POINTER;
+	num->base = 0;
+	num->bodyInt = NULL;
+	num->lenInt = 0;
+	num->bodyFrac = NULL;
+	num->lenFrac = 0;
+	return OK;
+}
+
+error delete_num(Num *num) {
+	if (num == NULL)
+		return NULL_POINTER;
+	free(num->bodyInt);
+	free(num->bodyFrac);
+	return OK;
+}
 
 int main() {
 	return OK;
