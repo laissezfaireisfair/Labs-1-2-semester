d76b34bf31b63dd4faf0f23db5fe17da1daa9341
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-03-07 10:25:16 +0700

    1-0 Update to find all coincidences

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -38,14 +38,12 @@ String ask_String(FILE* fin) { //TODO: Make it memory-friendly and not limited
   return str;
  }
 
-/// Returns 1 if founded, 0 if not founded, otherwise - error code
-int find_substr(String const substr, String const str, FILE* fout) {
+void find_substr(String const substr, String const str, FILE* fout) {
   unsigned int const maxNumOfSymbols = 256;
   assert(fout != NULL);
   assert(substr.length != 0);
   assert(str.length != 0);
-  if (substr.length > str.length)
-    return 2;
+  assert(substr.length <= str.length);
   int stopTable[maxNumOfSymbols]; // Distance from begin to first symbol position
   for (unsigned int i = 0; i < maxNumOfSymbols; ++i)
     stopTable[i] = -1;
@@ -56,8 +54,11 @@ int find_substr(String const substr, String const str, FILE* fout) {
       last = i; // Last checked symbol
     fprintf(fout, "%u ", i + 1);
     if (substr.body[j] == str.body[i]) {
-      if (j == 0)
-        return 1;
+      if (j == 0) {
+        i = last + substr.length;
+        j = substr.length - 1;
+        continue;
+      }
       --j;
       --i;
       continue;
@@ -68,7 +69,6 @@ int find_substr(String const substr, String const str, FILE* fout) {
       i = last + substr.length;
     j = substr.length - 1;
   }
-  return 0;
 }
 
 int main(void) {
@@ -86,12 +86,10 @@ int main(void) {
   }
   fclose(fin);
   FILE* fout = fopen("out.txt", "w");
-  int const status = find_substr(substr, str, fout);
+  find_substr(substr, str, fout);
   fprintf(fout, "\n");
   fclose(fout);
   free(substr.body);
   free(str.body);
-  if (status != 1 && status != 0)
-    return 3;
   return 0;
 }
