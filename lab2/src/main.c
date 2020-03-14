de48d539c2584b32110ae61fa248ddaa55f7a36a
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-03-14 15:42:01 +0700

    2 Add main draft

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -1,23 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "Errors.h"
+#include "Sequence.h"
+#include "Permutations.h"
 
-int const          TRUE             = 1;
-int const          FALSE            = 0;
-char const * const INPUT_FILE_NAME  = "in.txt";
-char const * const OUTPUT_FILE_NAME = "out.txt";
+int main() {
+  int const          MAX_SEQ_LEN      = 10;
+  char const * const INPUT_FILE_NAME  = "in.txt";
+  char const * const OUTPUT_FILE_NAME = "out.txt";
 
-enum errors {OK, DIVISION_BY_ZERO, NULL_POINTER, INVALID_ARGUMENT, BAD_INPUT,
-	 						RUNTIME_ERROR, LENGTH_ERROR};
-typedef enum errors error;
+  Sequence seq = make_seq();
 
-void print_error() {
-	FILE *fout = fopen(OUTPUT_FILE_NAME, "w");
-	if (fout == NULL)
-		exit(RUNTIME_ERROR);
-	fprintf(fout, "bad input\n");
-	fclose(fout);
-}
+  FILE *fin = fopen(INPUT_FILE_NAME, "r");
+  if (fin == NULL) {
+    print_error(OUTPUT_FILE_NAME, RUNTIME_ERROR);
+    return RUNTIME_ERROR;
+  }
 
-int main(void) {
-    return OK;
+  error const readStatus = ask_sequence(fin, &seq, MAX_SEQ_LEN);
+  if (readStatus != OK) {
+    print_error(OUTPUT_FILE_NAME, readStatus);
+    return readStatus;
+  }
+
+  unsigned int num;
+  if (fscanf(fin, "%u", num) == 0) {
+    print_error(OUTPUT_FILE_NAME, BAD_INPUT);
+    return BAD_INPUT;
+  }
+  fclose(fin);
+
+  FILE *fout = fopen(OUTPUT_FILE_NAME, "r");
+  if (fout == NULL) {
+    print_error("error.txt", RUNTIME_ERROR);
+    return RUNTIME_ERROR;
+  }
+
+  error const printStatus = print_next_permutations(fout, seq, num);
+  if (printStatus != OK) {
+    print_error(OUTPUT_FILE_NAME, printStatus);
+    return printStatus;
+  }
+  fclose(fout);
+
+  return OK;
 }
