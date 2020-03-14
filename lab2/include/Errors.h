75e6e6fd911cec6af049efd2f11f61b54dd5c677
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-03-14 09:52:05 +0700

    2 Move error handling to separate module

---
+++ lab2/include/Errors.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum errors {OK, DIVISION_BY_ZERO, NULL_POINTER, INVALID_ARGUMENT, BAD_INPUT,
+	 						RUNTIME_ERROR, LENGTH_ERROR};
+typedef enum errors error;
+
+void print_error(char *filename, error const err);
---
+++ lab2/src/Errors.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "Errors.h"
+
+void print_error(char *filename, error const err) {
+	FILE *fout = fopen(filename, "w");
+	if (fout == NULL)
+		exit(RUNTIME_ERROR);
+  switch(err) {
+    case(OK):               fprintf(fout, "ok\n"); break;
+    case(DIVISION_BY_ZERO): fprintf(fout, "division by zero\n"); break;
+    case(NULL_POINTER):     fprintf(fout, "null pointer\n"); break;
+    case(INVALID_ARGUMENT): fprintf(fout, "invalid argument\n"); break;
+    case(BAD_INPUT):        fprintf(fout, "bad input\n"); break;
+    case(RUNTIME_ERROR):    fprintf(fout, "runtime error\n"); break;
+    case(LENGTH_ERROR):     fprintf(fout, "length error\n"); break;
+  }
+	fclose(fout);
+}
