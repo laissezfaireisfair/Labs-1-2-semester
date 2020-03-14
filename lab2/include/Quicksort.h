77b2d9666f209cef122ee782654e42fc83921d7c
Andrey Golubev <a.golubev1@g.nsu.ru>
2020-03-14 10:26:13 +0700

    2 Add quicksort module (from lab3)

---
+++ lab2/include/Quicksort.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Errors.h"
+
+error swap(int *a, int *b);
+
+error sort(int* arr, unsigned int const len);
---
+++ lab2/src/Quicksort.c
@@ -0,0 +1,34 @@
+#include "Errors.h"
+
+error swap(int *a, int *b) {
+  if (a == NULL || b == NULL)
+    return NULL_POINTER;
+  *a ^= *b;
+  *b ^= *a;
+  *a ^= *b;
+  return OK;
+}
+
+error sort(int* arr, unsigned int const len) {
+  if (arr == NULL)
+    return NULL_POINTER;
+  if (len < 2)
+    return OK;
+  int const midVal = arr[len / 2];
+  int left = 0, right = len - 1;
+  while (left <= right) {
+    for (; arr[left] < midVal; ++left);
+    for (; arr[right] > midVal; --right);
+    if (left <= right) {
+      if (arr[left] > arr[right]) {
+        error swapStatus = swap(arr+left, arr+right);
+        if (swapStatus != OK)
+          return swapStatus;
+      }
+      ++left;
+      --right;
+    }
+  }
+  return sort(arr, right+1);
+  return sort(arr + left, len - left);
+}
